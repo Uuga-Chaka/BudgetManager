c7dbf10b0ca2e06718147a29e61e2dab58572576
uuga <jpgarias09@gmail.com>
2026-01-06 00:44:23 -0500

    chore: :label: add navigation types for screen components and hooks

--- src/navigation/navigation.types.ts
+++ src/navigation/navigation.types.ts
@@ -1,19 +1,35 @@
+import {
+  type BottomTabNavigationProp,
+  type BottomTabScreenProps,
+} from '@react-navigation/bottom-tabs';
+import {
+  type NavigatorScreenParams,
+  type CompositeNavigationProp,
+  type CompositeScreenProps,
+} from '@react-navigation/native';
+import {
+  type NativeStackNavigationProp,
+  type NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+
 export const Routes = {
-  // MAIN
+  // ROOT
   Onboarding: 'Onboarding',
   Home: 'Home',
+
   // TAB
   Dashboard: 'Dashboard',
   Transactions: 'Transactions',
   Planning: 'Planning',
+
   // ONBOARDING
   Introduction: 'Introduction',
   IncomeSetup: 'IncomeSetup',
-};
+} as const;
 
 export type RootStackParamList = {
-  [Routes.Onboarding]: undefined;
-  [Routes.Home]: undefined;
+  [Routes.Onboarding]: NavigatorScreenParams<OnboardingParamList>;
+  [Routes.Home]: NavigatorScreenParams<TabStackParamList>;
 };
 
 export type TabStackParamList = {
@@ -26,3 +42,34 @@ export type OnboardingParamList = {
   [Routes.Introduction]: undefined;
   [Routes.IncomeSetup]: undefined;
 };
+
+// types for screen components props
+export type RootOnboardingScreenProps<RouteName extends keyof OnboardingParamList> =
+  CompositeScreenProps<
+    NativeStackScreenProps<OnboardingParamList, RouteName>,
+    NativeStackScreenProps<RootStackParamList>
+  >;
+
+export type RootTabScreenProps<RouteName extends keyof TabStackParamList> = CompositeScreenProps<
+  BottomTabScreenProps<TabStackParamList, RouteName>,
+  CompositeScreenProps<
+    NativeStackScreenProps<OnboardingParamList>,
+    NativeStackScreenProps<RootStackParamList>
+  >
+>;
+
+// types when annotating navigation directly
+export type RootOnboardingScreenNavigationProps<RouteName extends keyof OnboardingParamList> =
+  CompositeNavigationProp<
+    NativeStackNavigationProp<OnboardingParamList, RouteName>,
+    NativeStackNavigationProp<RootStackParamList>
+  >;
+
+export type RootTabScreenNavigationProps<RouteName extends keyof TabStackParamList> =
+  CompositeNavigationProp<
+    BottomTabNavigationProp<TabStackParamList, RouteName>,
+    CompositeNavigationProp<
+      NativeStackNavigationProp<OnboardingParamList>,
+      NativeStackNavigationProp<RootStackParamList>
+    >
+  >;
