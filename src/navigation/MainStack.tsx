c7c808fbd5d5f36f0ce14527b9eae783f27aba17
uuga <jpgarias09@gmail.com>
2026-01-09 15:25:10 -0500

    refactor: :recycle: moved background styles to app component

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,7 +1,6 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {StyleService, useTheme} from '@ui-kitten/components';
-import {StatusBar, View} from 'react-native';
+import {StyleService} from '@ui-kitten/components';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import HomeTabs from './HomeTabs';
@@ -11,41 +10,30 @@ import {useAppTheme} from '../hooks/useAppTheme';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-const styleProps = (bg: string) =>
-  StyleService.create({
-    safeAreaContainer: {
-      flex: 1,
-    },
-    container: {
-      flex: 1,
-      backgroundColor: bg,
-    },
-  });
+const styles = StyleService.create({
+  safeAreaContainer: {
+    flex: 1,
+  },
+});
 
 export default function Router() {
-  const theme = useTheme();
   const {statusBarStyle} = useAppTheme();
 
-  const styles = styleProps(theme['background-basic-color-1']);
-
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <StatusBar barStyle="default" translucent />{' '}
-        <SafeAreaView style={styles.safeAreaContainer}>
-          <Stack.Navigator
-            initialRouteName={Routes.Onboarding}
-            screenOptions={{
-              headerShown: false,
-              statusBarStyle: statusBarStyle,
-            }}>
-            <Stack.Screen name={Routes.Home} component={HomeTabs} />
-            <Stack.Group>
-              <Stack.Screen name={Routes.Onboarding} component={OnboardingRouter} />
-            </Stack.Group>
-          </Stack.Navigator>
-        </SafeAreaView>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        <Stack.Navigator
+          initialRouteName={Routes.Onboarding}
+          screenOptions={{
+            headerShown: false,
+            statusBarStyle: statusBarStyle,
+          }}>
+          <Stack.Screen name={Routes.Home} component={HomeTabs} />
+          <Stack.Group>
+            <Stack.Screen name={Routes.Onboarding} component={OnboardingRouter} />
+          </Stack.Group>
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
